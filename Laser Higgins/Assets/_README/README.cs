5205f90dc8145cfde469e4ec09be0800f8755e77
alexdzius <akeldwithaviation@gmail.com>
2020-05-15 14:51:20 -0700

    rgrr

---
+++ Laser Higgins/Assets/_README/README.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        - make your own level for testing
+        - final products will not have the following name convention
+        - anything you create must have your first letter of your first name as the first char
+        [for example: aPlayerController if alex would be making it]
+        everyone will have their own script folder, for convienience
+        CHECK TRELLO TO SEE IF SOMEONE ELSE IS NOT MAKING SOMETHING ALREADY
+        WE ALREADY WORK A LOT DONT MAKE DUPLICATES LMAO
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
