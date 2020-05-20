a22c0296859c33d8ab2692ef4d112ad0d79dc4c4
alexdzius <akeldwithaviation@gmail.com>
2020-05-20 14:46:27 -0700

    timebar and timeslowdown implemented

---
+++ Laser Higgins/Assets/Scripts/otherTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otherTest : MonoBehaviour
+{
+    Rigidbody2D myRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yMovement = Input.GetAxisRaw("Vertical");
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        Vector2 move = new Vector2(xMovement, yMovement).normalized * GameManager.normalTime;
+        transform.position += (Vector3)move;
+    }
+}
