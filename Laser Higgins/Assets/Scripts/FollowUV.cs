65051fee3909b2a9a3a7c3b23cbdcc050f342ffe
alexdzius <akeldwithaviation@gmail.com>
2020-05-29 14:34:00 -0700

    backgrounds in

---
+++ Laser Higgins/Assets/Scripts/FollowUV.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUV : MonoBehaviour
+{
+    public float parallax = 2f;
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offeset = mat.mainTextureOffset;
+        offeset.x = transform.position.x / transform.localScale.x/parallax;
+        offeset.y = transform.position.y / transform.localScale.y/parallax;
+        mat.mainTextureOffset = offeset;
+    }
+}
---
+++ Laser Higgins/Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+    Rigidbody2D go;
+    void Start()
+    {
+       go = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = go.transform.position;
+        go.velocity = new Vector3(0, -1, 0);
+        if(position.y < -10)
+        {
+            go.transform.position = new Vector3(0, 20, 0);
+        }
+    }
+}
