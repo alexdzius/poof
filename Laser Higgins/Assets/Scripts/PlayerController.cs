b6b2221239d8287a558254176230e9acf1753f19
marvin-digipen <56563618+marvin-digipen@users.noreply.github.com>
2020-05-29 13:44:16 -0700

    new projectile behavior

--- Laser Higgins/Assets/Scripts/PlayerController.cs
+++ Laser Higgins/Assets/Scripts/PlayerController.cs
@@ -13,14 +13,15 @@ public class PlayerController : MonoBehaviour
   public Joystick joystick;
   public GameObject gameobject;
   public float timer = 5;
+  Vector3 currentDirection;
   // Start is called before the first frame update
   void Start()
   {
-        Time.timeScale = GameManager.timeScaleAdjuster;
+    Time.timeScale = GameManager.timeScaleAdjuster;
 #if !UNITY_IOS
-        gameobject.SetActive(false);
+    gameobject.SetActive(false);
 #endif
-    }
+  }
 
   // Update is called once per frame
   void Update()
@@ -35,11 +36,11 @@ public class PlayerController : MonoBehaviour
             xMovement = joystick.Horizontal;
         }
 #endif
-        Vector2 move = new Vector2(xMovement, yMovement).normalized * Time.deltaTime * speed;
-        transform.position += (Vector3)move;
-        Time.timeScale = GameManager.timeScaleAdjuster;
-        // shoot
-        timeSinceLastFire += Time.deltaTime;
+    currentDirection = new Vector2(xMovement, yMovement).normalized * speed;
+    transform.position += (Vector3)currentDirection * Time.deltaTime;
+    Time.timeScale = GameManager.timeScaleAdjuster;
+    // shoot
+    timeSinceLastFire += Time.deltaTime;
     if (Input.GetKey("space"))
     {
       if (timeSinceLastFire > 1 / fireRate)
@@ -88,27 +89,29 @@ public class PlayerController : MonoBehaviour
 #endif
 
 
-    }
+  }
 
   void OnCollisionEnter2D(Collision2D collision)
   {
     ProjectileController controller = collision.gameObject.GetComponent<ProjectileController>();
     if (controller != null)
     {
-      if(controller.type == ProjectileController.Type.Enemy){
+      if (controller.type == ProjectileController.Type.Enemy)
+      {
         // if it hits an enemy projectile, then decrease lives and then destroy the bullet
         GameManager.TotalLifes--;
-                SoundEffectHandler.damaged = true;
+        SoundEffectHandler.damaged = true;
         controller.Destroy();
       }
     }
     // check loop to check whether the object collided is certain powerup
 
     // if you have hit the health powerup
-    if(collision.gameObject.tag == "HealthPUP")
+    if (collision.gameObject.tag == "HealthPUP")
     {
       // if you have collided and theres less than 3 lifes
-      if(GameManager.TotalLifes < 3){
+      if (GameManager.TotalLifes < 3)
+      {
         // add a life
         GameManager.TotalLifes++;
       }
@@ -116,57 +119,60 @@ public class PlayerController : MonoBehaviour
       Destroy(collision.gameObject);
     }
     // if you have hit the firerate powerup
-    if(collision.gameObject.tag == "FirePUP"){
-        // destroy the collided object
-        Destroy(collision.gameObject);
-        // set the firerate to 6, increasing by 50%
-        fireRate = 6f;
-        // set a temporary delay timer for 5s
-        while (timer > 0){
-          timer -= Time.deltaTime;
-        }
-        // set time back
-        timer = 5;
-        // reset fire rate
-        fireRate = 4f;
+    if (collision.gameObject.tag == "FirePUP")
+    {
+      // destroy the collided object
+      Destroy(collision.gameObject);
+      // set the firerate to 6, increasing by 50%
+      fireRate = 6f;
+      // set a temporary delay timer for 5s
+      while (timer > 0)
+      {
+        timer -= Time.deltaTime;
+      }
+      // set time back
+      timer = 5;
+      // reset fire rate
+      fireRate = 4f;
     }
-    if(collision.gameObject.tag == "BlockPUP"){
+    if (collision.gameObject.tag == "BlockPUP")
+    {
       // destrouy colliding object
       Destroy(collision.gameObject);
       // spawn block
       Instantiate(theBlock, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), Quaternion.identity);
     }
-    if(collision.gameObject.tag == "TimePUP")
-        {
-            // destroy colliding object
-            Destroy(collision.gameObject);
-            // set the timers to be beneficial
-            GameManager.timeLeft = 5f;
-            WaveController.wavetimer += 11f;
-            WaveController.powertimer -= 11f;
-        }
-    if(collision.gameObject.tag == "DeathPUP")
-        {
-            // destroy object
-            Destroy(collision.gameObject);
-            // do some funky wunky
-            GameManager.TotalLifes--;
-            SoundEffectHandler.damaged = true;
-            WaveController.wavetimer -= 5f;
-        }
-    if(collision.gameObject.tag == "Enemy")
-        {
-            // delete enemy
-            Destroy(collision.gameObject);
-            // remove life
-            GameManager.TotalLifes--;
-            SoundEffectHandler.damaged = true;
-        }
-    if(collision.gameObject.tag == "FreeTime")
-        {
-            Destroy(collision.gameObject);
-            GameManager.timeLeft = 5f;
-        }
+    if (collision.gameObject.tag == "TimePUP")
+    {
+      // destroy colliding object
+      Destroy(collision.gameObject);
+      // set the timers to be beneficial
+      GameManager.timeLeft = 5f;
+      WaveController.wavetimer += 11f;
+      WaveController.powertimer -= 11f;
+    }
+    if (collision.gameObject.tag == "DeathPUP")
+    {
+      // destroy object
+      Destroy(collision.gameObject);
+      // do some funky wunky
+      GameManager.TotalLifes--;
+      SoundEffectHandler.damaged = true;
+      WaveController.wavetimer -= 5f;
+    }
+    if (collision.gameObject.tag == "Enemy")
+    {
+      // delete enemy
+      Destroy(collision.gameObject);
+      // remove life
+      GameManager.TotalLifes--;
+      SoundEffectHandler.damaged = true;
+    }
+    if (collision.gameObject.tag == "FreeTime")
+    {
+      Destroy(collision.gameObject);
+      GameManager.timeLeft = 5f;
+    }
   }
   // fire a plasma something
   void Fire()
@@ -175,6 +181,11 @@ public class PlayerController : MonoBehaviour
     bullet.GetComponent<ProjectileController>().type = ProjectileController.Type.Player;
     // teleport bullet to player
     bullet.transform.position = transform.position;
-        SoundEffectHandler.shooted = true;
+    SoundEffectHandler.shooted = true;
+  }
+
+  public Vector3 getCurrentDirection()
+  {
+    return currentDirection;
   }
 }
