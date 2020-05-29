8bb0863004d427a7ef8bfebd9f49a8494145169c
alexdzius <akeldwithaviation@gmail.com>
2020-05-29 14:58:28 -0700

    small adjustments

---
+++ Laser Higgins/Assets/Scripts/TextController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextController : MonoBehaviour
+{
+    public Text[] texts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        texts = GetComponentsInChildren<Text>();
+#if UNITY_IOS
+        Color zm = texts[1].color;
+        zm.a = 0.0f;
+        texts[1].color = zm;
+#endif
+#if !UNITY_IOS
+        Color zm = texts[0].color;
+        zm.a = 0.0f;
+        texts[0].color = zm;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
