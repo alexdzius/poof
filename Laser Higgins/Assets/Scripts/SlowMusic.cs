d3f46d024e974ae52d875cac006855db56e45a94
Kyle-2 <48459007+Kyle-2@users.noreply.github.com>
2020-06-01 13:21:13 -0700

    added E audio and made SlowMusic.cs

---
+++ Laser Higgins/Assets/Scripts/SlowMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMusic : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.TheTimer)
+        {
+            this.GetComponent<AudioSource>().pitch = (1.0f / 6.0f);
+        }
+        else
+        {
+            this.GetComponent<AudioSource>().pitch = 1.0f;
+        }
+    }
+}
